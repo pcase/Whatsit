8754bc91936a96f62a5d29c0adf1b8d531645386
Patty Case <beadypatty@yahoo.com>
2018-10-20 13:56:29 -0700

    Initial Commit

---
+++ Whatsit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Whatsit
+//
+//  Created by Patty Case on 10/20/18.
+//  Copyright © 2018 Azure Horse Creations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
